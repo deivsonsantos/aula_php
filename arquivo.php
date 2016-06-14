6e7de581c5b8958c9fec3421932827db9263a221
deivsonsantos <dvs.santos@gmail.com>
2016-06-14 20:57:59 -0300

    terceiro commit

--- arquivo.php
+++ arquivo.php
@@ -1,7 +1,20 @@
 <?php
 
+$lista = ["Deivson","Santos", 16];
+$variavel = "nome";
 
-$variavel = "primeira variavel";
-echo $variavel;
+
+echo $lista[0];
+
+// imprimindo mais de um valor com Foreach
+
+foreach ($lista as $valor) {
+
+	if ($valor == 16){
+
+		echo $valor;
+	}
+	
+}
 
 ?>
